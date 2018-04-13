395334342d61c221618ca2f05c6c8efcf67b2d4f
Kyle Tyler <tylerkyl@flip1.engr.oregonstate.edu>
2018-04-12 20:20:11 -0700

    Fixed the last warning

--- projects/tylerkyl/dominion/dominion.c
+++ projects/tylerkyl/dominion/dominion.c
@@ -659,7 +659,9 @@ int playSmithy(struct gameState *state, int handPos, int currentPlayer) {
 }
 
 // New function for adventurer implementation
-int playAdventurer(struct gameState *state, int handPos, int currentPlayer, int drawntreasure, int cardDrawn, int z, int temphand[]) {
+int playAdventurer(struct gameState *state, int handPos, int currentPlayer, int drawntreasure, int z, int temphand[]) {
+    int cardDrawn;
+
     while(drawntreasure < 2) {
         if(state->deckCount[currentPlayer] < 1) { // If the deck is empty we need to shuffle discard and add to deck
             shuffle(currentPlayer, state);
@@ -755,7 +757,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
   int drawntreasure=0;
-  int cardDrawn;
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
@@ -767,7 +768,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     {
     case adventurer:
 	
-        playAdventurer(state, handPos, currentPlayer, drawntreasure, cardDrawn, z, temphand);
+        playAdventurer(state, handPos, currentPlayer, drawntreasure, z, temphand);
         return 0;
 
     case council_room:
