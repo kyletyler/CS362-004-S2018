ae5f055d55a8c5bbd2bc0ae69588b29498269f37
Kyle Tyler <tylerkyl@flip3.engr.oregonstate.edu>
2018-04-27 19:27:15 -0700

    rough cardtest1 done

--- projects/tylerkyl/dominion/cardtest1.c
+++ projects/tylerkyl/dominion/cardtest1.c
@@ -14,6 +14,8 @@
  *  No state change should occur to the victory card piles and kingdom card piles
  */
 
+// Remember: There are bugs in the smithy card!
+// It draws the wrong number of cards and doesn't discard
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
@@ -35,7 +37,9 @@ int main() {
     int seed = 1000;
     int numPlayers = 2;
     int thisPlayer = 0;
-    struct gameStateG, test_G;
+    int otherPlayer = 1;
+    struct gameState G, test_G;
+    int test_k[10];
     int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
 
     // initialize a game state and player cards
@@ -49,5 +53,66 @@ int main() {
     
     // Copy the game state to the test game state
     memcpy(&test_G, &G, sizeof(struct gameState));
+    cardEffect(smithy, choice1, choice2, choice3, &test_G, handPos, &bonus);
 
+    newCards = 3;
+    printf("hand count = %d, expected = %d\n", test_G.handCount[thisPlayer], G.handCount[thisPlayer]+ newCards - discarded);
+    //assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
+    
+    ///////////////////////////////////////////////////////////
+    // TEST 2: Cards recieved are from current player's pile //
+    ///////////////////////////////////////////////////////////
+    
+    // Copy the game state to the test game state
+    memcpy(&test_G, &G, sizeof(struct gameState));
+    cardEffect(smithy, choice1, choice2, choice3, &test_G, handPos, &bonus);
+    
+    printf("deck count = %d, expected = %d\n", test_G.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
+   // assert(test_G.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards + shuffledCards);
 
+    ///////////////////////////////////////////////
+    // TEST 3: No state change for other players //
+    ///////////////////////////////////////////////
+    
+    // Copy the game state to the test game state
+    memcpy(&test_G, &G, sizeof(struct gameState));
+
+    cardEffect(smithy, choice1, choice2, choice3, &test_G, handPos, &bonus);
+   
+
+    printf("otherPlayer handCount = %d, expected = %d\n", test_G.handCount[otherPlayer], G.handCount[otherPlayer]);
+    printf("otherPlayer deckCount = %d, expected = %d\n", test_G.deckCount[otherPlayer], G.deckCount[otherPlayer]);
+    printf("otherPlayer discardCount = %d, expected = %d\n", test_G.discardCount[otherPlayer], G.discardCount[otherPlayer]);
+/*
+    if(test_G.handCount[otherPlayer] == G.handCount[otherPlayer] &&
+       test_G.deckCount[otherPlayer] == G.deckCount[otherPlayer] &&
+       test_G.discardCount[otherPlayer] == G.discardCount[otherPlayer]) {
+        printf("\n");
+    }
+    else {
+        printf("nah\n");
+    }
+*/
+
+    //////////////////////////////////////////////////////////////
+    // TEST 4: No state change to victory or kingdom card piles //
+    //////////////////////////////////////////////////////////////
+    
+    // Copy the game state to the test game state
+    memcpy(&test_G, &G, sizeof(struct gameState));
+    memcpy(&test_k, &k, sizeof(k));
+
+    cardEffect(smithy, choice1, choice2, choice3, &test_G, handPos, &bonus);
+
+    // Checking changes to victory card piles
+    printf("estate count = %d, expected = %d\n", test_G.supplyCount[estate], G.supplyCount[estate]);
+    printf("duchy count = %d, expected = %d\n", test_G.supplyCount[duchy], G.supplyCount[duchy]);
+    printf("province count = %d, expected = %d\n", test_G.supplyCount[province], G.supplyCount[province]);
+     
+    // Checking changes to kingdom card piles
+    for(i = 0; i < 10; i++)  {
+        printf("kingdom card %d count = %d, expected = %d\n", i, k[i], test_k[i]);
+    }
+
+    return 0;
+}
